03eae142257b8c5e0a3b570a4a897330a1540679
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-07-25 01:07:17 +0000

    Added methods to gram controller to make form worm

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -6,8 +6,13 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
-  # def create
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
 
-  # end
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 
 end
