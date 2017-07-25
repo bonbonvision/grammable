39a02309ae51ca4c619a1d49d3cd4787e89202e3
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-07-25 23:34:14 +0000

    Added show page for gram

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+
+  describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :show, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return 404 error if the gram is not found" do
+      get :show, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+
   describe "grams#index action" do
+
     it "should successfully show the page" do
       get :index
       expect(response).to have_http_status(:success)
@@ -9,6 +25,7 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#new action" do
+
     it "should require users to be logged in" do
       get :new
       expect(response).to redirect_to new_user_session_path
@@ -25,10 +42,12 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#create action" do
+
     it "should require users to be logged in" do
      post :create, params: { gram: { message: "Hello" } }
      expect(response).to redirect_to new_user_session_path
   end
+
     it "should successfully create a new gram in our database" do
       user = FactoryGirl.create(:user)
       sign_in user
@@ -45,9 +64,11 @@ RSpec.describe GramsController, type: :controller do
       user = FactoryGirl.create(:user)
       sign_in user
 
+      gram_count = Gram.count
       post :create, params: { gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq 0
+      expect(gram_count).to eq Gram.count
     end
+
   end
 end
