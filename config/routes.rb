c06410b9816b91f93599e407c0d15e882d74bf5a
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-08-04 11:46:42 +0000

    created comments on grams

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "grams#index"
   devise_for :users
-  resources :grams
+  resources :grams do
+    resources :comments, only: :create
+  end
 end
